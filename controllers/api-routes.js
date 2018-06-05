658989e7518f1286df0a5092ad9f9c67e52c8def
Alexandria Szoeke <alex_szoeke@outlook.com>
2018-06-04 22:38:06 -0500

    new scrape, still on progress

--- controllers/api-routes.js
+++ controllers/api-routes.js
@@ -1,6 +1,7 @@
 var db = require("../models");
 var axios = require("axios");
 var cheerio = require("cheerio");
+var  Handlebars = require('handlebars');
 
 
 
@@ -8,28 +9,35 @@ module.exports = function (app) {
 
   app.get("/scrape", function (req, res) {
     // First, we grab the body of the html with request
-    axios.get("https://www.cnn.com/entertainment").then(function (response) {
+    axios.get("https://www.washingtonpost.com/goingoutguide/music/?utm_term=.4c2151c1b179").then(function (response) {
       // Then, we load that into cheerio and save it to $ for a shorthand selector
       var $ = cheerio.load(response.data);
 
       // Now, we grab every h2 within an article tag, and do the following:
-      $("h3").each(function (i, element) {
+      $(".story-body").each(function (i, element) {
         // Save an empty result object
         var result = {};
 
         // Add the text and href of every link, and save them as properties of the result object
         result.title = $(this)
-          .children("a")
-          .text();
+          .children("h3");
+          // .text();
+          console.log(result.title);
+        result.summary = $(this)
+          .children("p")
+          .attr("data-pb-local-content-field");
+          
         result.link = $(this)
           .children("a")
           .attr("href");
+          
 
         // Create a new Article using the `result` object built from scraping
         db.Article.create(result)
           .then(function (dbArticle) {
             // View the added result in the console
             console.log(dbArticle);
+          
           })
           .catch(function (err) {
             // If an error occurred, send it to the client
--- public/app.js
+++ public/app.js
@@ -1,80 +1,92 @@
-$(document).ready(function() {
-// Grab the articles as a json
-$.getJSON("/articles", function(data) {
-  // For each one
-  for (var i = 0; i < data.length; i++) {
-    // Display the apropos information on the page
-    $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
-  }
-});
+$(document).ready(function () {
 
 
-// Whenever someone clicks a p tag
-$(document).on("click", "p", function() {
-  // Empty the notes from the note section
-  $("#notes").empty();
-  // Save the id from the p tag
-  var thisId = $(this).attr("data-id");
+  $.getJSON("/articles", function(data) {
+    console.log(data);
+    // return data;
+    // For each one
+    // var newDiv = $("<div>").addClass('card');
+    // $(newDiv).appendTo("#articles");
+    for (var i = 0; i < 15; i++) {
+      // Display the apropos information on the page
+      // $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+      $("#articles").append("<div class='card'><div class='card-body'><h5 class='card-title' data-id='" + data[i]._id + "'>" + data[i].title + "</h5><p class='card-text'>" + data[i].summary + "</p><a href='" + data[i].link +"' class='card-link'>Read more!</a>" + "</p></div></div><br>");
 
-  // Now make an ajax call for the Article
-  $.ajax({
-    method: "GET",
-    url: "/articles/" + thisId
-  })
-    // With that done, add the note information to the page
-    .then(function(data) {
-      console.log(data);
-      // The title of the article
-      $("#notes").append("<h2>" + data.title + "</h2>");
-      // An input to enter a new title
-      $("#notes").append("<input id='titleinput' name='title' >");
-      // A textarea to add a new note body
-      $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
-      // A button to submit a new note, with the id of the article saved to it
-      $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
-
-      // If there's a note in the article
-      if (data.note) {
-        // Place the title of the note in the title input
-        $("#titleinput").val(data.note.title);
-        // Place the body of the note in the body textarea
-        $("#bodyinput").val(data.note.body);
-      }
-    });
-});
-
-// When you click the savenote button
-$(document).on("click", "#savenote", function() {
-  // Grab the id associated with the article from the submit button
-  var thisId = $(this).attr("data-id");
-
-  // Run a POST request to change the note, using what's entered in the inputs
-  $.ajax({
-    method: "POST",
-    url: "/articles/" + thisId,
-    data: {
-      // Value taken from title input
-      title: $("#titleinput").val(),
-      // Value taken from note textarea
-      body: $("#bodyinput").val()
+      console.log(data[i]);
     }
-  })
-    // With that done
-    .then(function(data) {
-      // Log the response
-      console.log(data);
-      // Empty the notes section
-      $("#notes").empty();
+  });
+
+
+
+
+  // // Whenever someone clicks a p tag
+  // $(document).on("click", "h3", function () {
+  //   // Empty the notes from the note section
+  //   $("#notes").empty();
+  //   // Save the id from the p tag
+  //   var thisId = $(this).attr("data-id");
+
+  //   // Now make an ajax call for the Article
+  //   $.ajax({
+  //       method: "GET",
+  //       url: "/articles/" + thisId
+  //     })
+  //     // With that done, add the note information to the page
+  //     .then(function (data) {
+  //       console.log(data);
+  //       // The title of the article
+  //       $("#notes").append("<h2>" + data.title + "</h2>");
+  //       // An input to enter a new title
+  //       $("#notes").append("<input id='titleinput' name='title' >");
+  //       // A textarea to add a new note body
+  //       $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
+  //       // A button to submit a new note, with the id of the article saved to it
+  //       $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
+
+  //       // If there's a note in the article
+  //       if (data.note) {
+  //         // Place the title of the note in the title input
+  //         $("#titleinput").val(data.note.title);
+  //         // Place the body of the note in the body textarea
+  //         $("#bodyinput").val(data.note.body);
+  //       }
+  //     });
+  // });
+
+  // // When you click the savenote button
+  // $(document).on("click", "#savenote", function () {
+  //   // Grab the id associated with the article from the submit button
+  //   var thisId = $(this).attr("data-id");
+
+  //   // Run a POST request to change the note, using what's entered in the inputs
+  //   $.ajax({
+  //       method: "POST",
+  //       url: "/articles/" + thisId,
+  //       data: {
+  //         // Value taken from title input
+  //         title: $("#titleinput").val(),
+  //         // Value taken from note textarea
+  //         body: $("#bodyinput").val()
+  //       }
+  //     })
+  //     // With that done
+  //     .then(function (data) {
+  //       // Log the response
+  //       console.log(data);
+  //       // Empty the notes section
+  //       $("#notes").empty();
+  //     });
+
+  //   // Also, remove the values entered in the input and textarea for note entry
+  //   $("#titleinput").val("");
+  //   $("#bodyinput").val("");
+  // });
+
+  $("#scrape").on("click", function scrape(Article) {
+    $.get("/scrape", function(data) {
+      window.location.reload(true);
     });
-
-  // Also, remove the values entered in the input and textarea for note entry
-  $("#titleinput").val("");
-  $("#bodyinput").val("");
-});
-
-$("#scrape").on("click", function scrape(Article) {
-  $.get("/scrape", Article);
-  window.location.href = "/";
-});
+    
+  });
 
 });
